ab9295d47306f2e2b4a61eba7528b488352393e4
Your Name <johnkamau106@gmail.com>
2025-05-28 10:23:53 +0300

    Refactor database configuration to use SQLALCHEMY_DATABASE_URL and improve engine initialization

--- health_simplified/health_simplified/database.py
+++ health_simplified/health_simplified/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Base
+from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///health_tracker.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./health_simplified.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
